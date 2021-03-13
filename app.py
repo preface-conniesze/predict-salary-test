9b4f85d0e8209812c6acc006a617112d1f5dc052
preface-conniesze <52767890+preface-conniesze@users.noreply.github.com>
2021-03-13 21:01:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+#importing libraries
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+from flask_ngrok import run_with_ngrok
 
+# creating the flask object
 app = Flask(__name__)
+
+#making your Flask app available upon running
+run_with_ngrok(app)
+
+#loading the model
 model = pickle.load(open('model.pkl', 'rb'))
 
+#create routes
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -19,9 +28,8 @@ def predict():
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
-
+    
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
